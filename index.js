2dd9819a6fb0b775a8962b7dfd4ea6c4746b90d2
Sonali <sonalidhikale0@gmail.com>
2023-01-14 11:48:12 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const express = require("express")
+const bodyparser = require("body-parser")
+const mongoose = require("mongoose")
+const jsonparser = bodyparser.json()
+const app = express()
+
+const port = process.env.PORT
+const mongoURL = process.env.DB
+
+const database = mongoose.connect(mongoURL)
+
+app.get("/",(req,res)=>{
+    res.send("I am SERVER")
+})
+
+app.listen(port,()=>{
+    console.log("App listening on port 5000")
+})
